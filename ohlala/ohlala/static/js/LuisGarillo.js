56c11cb69cccc22648e68321a479792529f08fce
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-07-21 20:17:48 -0400

    Complete footer

--- ohlala/ohlala/static/js/LuisGarillo.js
+++ ohlala/ohlala/static/js/LuisGarillo.js
@@ -1,3 +1,19 @@
+
+function handleFooter() {
+    let footerSection = document.querySelector(".hideable-section");
+    let footerBtn = document.querySelector("#expand-btn");
+    if (footerSection.classList.length > 1) {
+        footerSection.classList.remove("hidden");
+        footerBtn.textContent = "Colapsar";
+    }
+    else {
+        footerSection.classList.add("hidden");
+        footerBtn.textContent = "Expandir";
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Tu código aquí
+    let footerBtn = document.querySelector("#expand-btn");
+    console.log(footerBtn);
+    footerBtn.addEventListener("click", handleFooter);
 });
