1450af2b5853f57266745ed383efaccb3343a2ca
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-07-23 18:01:40 -0400

    Add carousel to homepage

--- ohlala/ohlala/static/js/LuisGarillo.js
+++ ohlala/ohlala/static/js/LuisGarillo.js
@@ -1,3 +1,4 @@
+let index = 0;
 
 function handleFooter() {
     let footerSection = document.querySelector("#hideable");
@@ -11,8 +12,49 @@ function handleFooter() {
     footerSection.classList.toggle("show");
 }
 
+function advance() {
+    let slides = document.querySelector("#slides");
+    let firstChild = slides.children[0];
+    let circles = document.querySelector("#circles");
+
+    firstChild.classList.toggle("selected");
+    slides.removeChild(firstChild);
+    slides.appendChild(firstChild);
+    slides.children[0].classList.toggle("selected");
+
+    circles.children[index].classList.toggle("selected");
+    index++;
+    if (index > 3) {
+        index = 0;
+    }
+    circles.children[index].classList.toggle("selected");
+}
+
+function goBack() {
+    let slides = document.querySelector("#slides");
+    let lastChild = slides.children[slides.children.length - 1];
+    let firstChild = slides.children[0];
+
+    slides.removeChild(lastChild);
+    firstChild.classList.toggle("selected");
+    slides.insertBefore(lastChild, firstChild);
+    lastChild.classList.toggle("selected");
+
+    circles.children[index].classList.toggle("selected");
+    index--;
+    if (index < 0) {
+        index = 3;
+    }
+    circles.children[index].classList.toggle("selected");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     let footerBtn = document.querySelector("#expand-btn");
-    console.log(footerBtn);
     footerBtn.addEventListener("click", handleFooter);
+
+    let nextBtn = document.querySelector("#next");
+    let prevBtn = document.querySelector("#prev");
+
+    nextBtn.addEventListener("click", advance);
+    prevBtn.addEventListener("click", goBack);
 });
