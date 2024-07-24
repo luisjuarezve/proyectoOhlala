4372279844f9e2253e5e97852aabb105a4db03d6
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-07-24 02:36:47 -0400

    Complete schedule date page

--- ohlala/ohlala/static/js/LuisGarillo.js
+++ ohlala/ohlala/static/js/LuisGarillo.js
@@ -1,4 +1,10 @@
 let index = 0;
+let pediServices = ["Pedicura básica", "Pedicura Spa"];
+let pediPrices = [600, 900];
+let maniServices = ["Manicura básica", "Manicura rusa", "Manicura en gel", "Aprés Gel X", "Dip Powder", "Polygel"];
+let maniPrices = [600, 900, 1000, 1200, 1500, 1200, 1200];
+let maniPrice = 0;
+let pediPrice = 0;
 
 function handleFooter() {
     let footerSection = document.querySelector("#hideable");
@@ -65,6 +71,82 @@ function configureDate(dateField) {
     dateField.setAttribute("max", `${rangeDate}-${month}-${day}`);
 }
 
+function setDate(event) {
+    let date = new Date(event.target.value);
+    let today = new Date()
+    if (date.getTime() < today.getTime()) {
+        document.querySelector("#date-error").textContent = "Solo se pueden agendar citas a partir de mañana.";
+        document.querySelector("#agendar-fecha").value = "0000-00-00";
+        return;
+    }
+    if (date.getDay() == 6) {
+        document.querySelector("#date-error").textContent = "No se trabajan los domingos.";
+        document.querySelector("#agendar-fecha").value = "0000-00-00";
+        return;
+    }
+    document.querySelector("#date-error").textContent = "";
+    document.querySelector("#confirm-date").textContent = event.target.value;
+}
+
+function setHour(event) {
+    let hour = event.target.value;
+    let time = "AM";
+    if (hour > 11) {
+        time = "PM";
+    }
+
+    if (hour > 12) {
+        hour -= 12;
+    }
+    document.querySelector("#confirm-hour").textContent = hour + ":00 " + time;
+}
+
+function setManicure(event) {
+    let value = event.target.value;
+    
+    if (value == 0) {
+        return;
+    }
+
+    document.querySelector("#confirm-mani").textContent = maniServices[value - 1];
+    document.querySelector("#mani-price").textContent = maniPrices[value - 1];
+
+    calculatePrice(maniPrices[value - 1], pediPrice);
+}
+
+function cancelMani() {
+    document.querySelector("#confirm-mani").textContent = "No se hará manicura";
+    document.querySelector("#mani-price").textContent = 0;
+    document.querySelector("#manicura").value = 0;
+    calculatePrice(0, pediPrice);
+}
+
+function cancelPedi() {
+    document.querySelector("#confirm-pedi").textContent = "No se hará pedicura";
+    document.querySelector("#pedi-price").textContent = 0;
+    document.querySelector("#pedicura").value = 0;
+    calculatePrice(maniPrice, 0);
+}
+
+function setPedicure(event) {
+    let value = event.target.value;
+    
+    if (value == 0) {
+        return;
+    }
+    
+    document.querySelector("#confirm-pedi").textContent = pediServices[value - 7];
+    document.querySelector("#pedi-price").textContent = pediPrices[value - 7];
+
+    calculatePrice(maniPrice, pediPrices[value - 7]);
+}
+
+function calculatePrice(mp, pp) {
+    maniPrice = mp;
+    pediPrice = pp;
+    document.querySelector("#total").textContent = maniPrice + pediPrice;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     let footerBtn = document.querySelector("#expand-btn");
     footerBtn.addEventListener("click", handleFooter);
@@ -80,5 +162,19 @@ document.addEventListener("DOMContentLoaded", function () {
     let dateField = document.querySelector("#date-field");
     if (dateField) {
         configureDate(dateField);
-    }   
+    }
+    
+    let dateDate = document.querySelector("#agendar-fecha");
+    let hora = document.querySelector("#hora");
+    let manicura = document.querySelector("#manicura");
+    let pedicura = document.querySelector("#pedicura");
+    let maniCancel = document.querySelector("#mani-cancel");
+    let pediCancel = document.querySelector("#pedi-cancel");
+
+    dateDate.addEventListener("input", setDate);
+    hora.addEventListener("input", setHour);
+    manicura.addEventListener("input", setManicure);
+    pedicura.addEventListener("input", setPedicure);
+    maniCancel.addEventListener("click", cancelMani);
+    pediCancel.addEventListener("click", cancelPedi);
 });
