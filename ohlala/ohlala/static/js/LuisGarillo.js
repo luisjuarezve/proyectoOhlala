cf4b981d9f7b860076490c07242e1b188b1bbbda
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-07-24 17:15:53 -0400

    Add disabled functionality to schedule date button

--- ohlala/ohlala/static/js/LuisGarillo.js
+++ ohlala/ohlala/static/js/LuisGarillo.js
@@ -5,6 +5,7 @@ let maniServices = ["Manicura básica", "Manicura rusa", "Manicura en gel", "Apr
 let maniPrices = [600, 900, 1000, 1200, 1500, 1200, 1200];
 let maniPrice = 0;
 let pediPrice = 0;
+let dateCheck, workerCheck, maniCheck, pediCheck = false;
 
 function handleFooter() {
     let footerSection = document.querySelector("#hideable");
@@ -84,8 +85,10 @@ function setDate(event) {
         document.querySelector("#agendar-fecha").value = "0000-00-00";
         return;
     }
+    dateCheck = true;
     document.querySelector("#date-error").textContent = "";
     document.querySelector("#confirm-date").textContent = event.target.value;
+    checkBtn();
 }
 
 function setHour(event) {
@@ -101,6 +104,21 @@ function setHour(event) {
     document.querySelector("#confirm-hour").textContent = hour + ":00 " + time;
 }
 
+function setManicurista(event) {
+    let value = event.target.value;
+    let manicuristas = ["Ana García", "Daniela Martínez", "María Rodríguez", "Antoniela Fernández", "Laura Gómez"];
+    workerCheck = true;
+    document.querySelector("#confirm-manicurista").textContent = manicuristas[value - 1];
+    checkBtn();
+}
+
+function cancelWork(event) {
+    document.querySelector("#confirm-manicurista").textContent = "Su manicurista";
+    document.querySelector("#manicurista").value = 0;
+    workerCheck = false;
+    checkBtn();
+}
+
 function setManicure(event) {
     let value = event.target.value;
     
@@ -110,22 +128,27 @@ function setManicure(event) {
 
     document.querySelector("#confirm-mani").textContent = maniServices[value - 1];
     document.querySelector("#mani-price").textContent = maniPrices[value - 1];
-
+    maniCheck = true;
     calculatePrice(maniPrices[value - 1], pediPrice);
+    checkBtn();
 }
 
 function cancelMani() {
     document.querySelector("#confirm-mani").textContent = "No se hará manicura";
     document.querySelector("#mani-price").textContent = 0;
     document.querySelector("#manicura").value = 0;
+    maniCheck = false;
     calculatePrice(0, pediPrice);
+    checkBtn();
 }
 
 function cancelPedi() {
     document.querySelector("#confirm-pedi").textContent = "No se hará pedicura";
     document.querySelector("#pedi-price").textContent = 0;
     document.querySelector("#pedicura").value = 0;
+    pediCheck = false;
     calculatePrice(maniPrice, 0);
+    checkBtn();
 }
 
 function setPedicure(event) {
@@ -137,14 +160,24 @@ function setPedicure(event) {
     
     document.querySelector("#confirm-pedi").textContent = pediServices[value - 7];
     document.querySelector("#pedi-price").textContent = pediPrices[value - 7];
-
+    pediCheck = true;
     calculatePrice(maniPrice, pediPrices[value - 7]);
+    checkBtn();
 }
 
 function calculatePrice(mp, pp) {
     maniPrice = mp;
     pediPrice = pp;
     document.querySelector("#total").textContent = maniPrice + pediPrice;
+}
+
+function checkBtn() {
+    if (dateCheck && workerCheck && (maniCheck || pediCheck)) {
+        document.querySelector("#schedule").disabled = false;
+    }
+    else {
+        document.querySelector("#schedule").disabled = true;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -166,15 +199,19 @@ document.addEventListener("DOMContentLoaded", function () {
     
     let dateDate = document.querySelector("#agendar-fecha");
     let hora = document.querySelector("#hora");
+    let manicurista = document.querySelector("#manicurista");
     let manicura = document.querySelector("#manicura");
     let pedicura = document.querySelector("#pedicura");
     let maniCancel = document.querySelector("#mani-cancel");
     let pediCancel = document.querySelector("#pedi-cancel");
+    let workCancel = document.querySelector("#work-cancel");
 
     dateDate.addEventListener("input", setDate);
     hora.addEventListener("input", setHour);
+    manicurista.addEventListener("input", setManicurista);
     manicura.addEventListener("input", setManicure);
     pedicura.addEventListener("input", setPedicure);
     maniCancel.addEventListener("click", cancelMani);
     pediCancel.addEventListener("click", cancelPedi);
+    workCancel.addEventListener("click", cancelWork);
 });
